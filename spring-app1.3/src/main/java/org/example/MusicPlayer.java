7296cb4b7632479ecd47ad7e625d88e5d817052c
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-06-18 18:58:06 +0300

    Add files via upload

---
+++ spring-app1.3/src/main/java/org/example/MusicPlayer.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class MusicPlayer {
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
+
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
+    }
+
+    public void playMusic() {
+        System.out.println(classicalMusic.getSong());
+        System.out.println(rockMusic.getSong());
+    }
+}
+
