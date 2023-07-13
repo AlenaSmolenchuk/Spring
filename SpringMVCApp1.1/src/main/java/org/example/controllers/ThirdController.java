066e094ef04f99ebf5e0700667073e24bf942a74
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-07-13 18:45:10 +0300

    Create ThirdController.java

---
+++ SpringMVCApp1.1/src/main/java/org/example/controllers/ThirdController.java
@@ -0,0 +1,34 @@
+package web.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Objects;
+
+@Controller
+@RequestMapping("/third")
+public class ThirdController {
+
+    @GetMapping("/calculator")
+    public String calculate(@RequestParam(value = "a", required = false) int a,
+                            @RequestParam(value = "b",required = false) int b,
+                            @RequestParam(value = "action",required = false) String action,
+                            Model model) {
+        if (Objects.equals(action, "multiplication")){
+            model.addAttribute("action", a * b);
+        } else if (Objects.equals(action, "addition")) {
+            model.addAttribute("action", a + b);
+        } else if (Objects.equals(action, "subtraction")) {
+            model.addAttribute("action", a - b);
+        } else if (Objects.equals(action, "division")) {
+            model.addAttribute("action", a / (double) b);
+        } else {
+            model.addAttribute("action", "Something is wrong");
+        }
+
+        return "third/calculator";
+    }
+}
