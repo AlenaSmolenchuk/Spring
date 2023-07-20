9374864fa313e13a24f43a472c6023d4cff8f1d9
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-07-20 22:41:00 +0300

    Update PeopleController.java

--- CRUD_App1/src/main/java/org/example/controllers/PeopleController.java
+++ CRUD_App1/src/main/java/org/example/controllers/PeopleController.java
@@ -53,7 +53,6 @@ public class PeopleController {
     @GetMapping("/{id}/edit")
     public String edit(Model model,
                        @PathVariable("id") int id) {
-
         model.addAttribute("person", personDAO.show(id));
         return "people/edit";
     }
