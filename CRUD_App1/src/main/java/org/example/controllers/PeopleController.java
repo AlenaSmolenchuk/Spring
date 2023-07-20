84b6c28cedf39f9c1bb0d5628a9fa237904ee143
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-07-20 22:41:12 +0300

    Update PeopleController.java

--- CRUD_App1/src/main/java/org/example/controllers/PeopleController.java
+++ CRUD_App1/src/main/java/org/example/controllers/PeopleController.java
@@ -71,9 +71,7 @@ public class PeopleController {
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") int id) {
-
         personDAO.delete(id);
-
         return "redirect:/people";
     }
 }
