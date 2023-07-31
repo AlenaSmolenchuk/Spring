7ccae52de0d58137b1aee46520295d4ace567727
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-07-31 23:47:59 +0300

    Update PeopleController.java

--- CRUD_App1(jdbc template)/src/main/java/org/example/controllers/PeopleController.java
+++ CRUD_App1(jdbc template)/src/main/java/org/example/controllers/PeopleController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
- 
+  
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
