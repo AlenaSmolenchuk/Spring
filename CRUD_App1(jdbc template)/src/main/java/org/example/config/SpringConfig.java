42f387b279776d3b046462cc389ab318bdb155ff
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-08-03 22:22:31 +0300

    Update SpringConfig.java

--- CRUD_App1(jdbc template)/src/main/java/org/example/config/SpringConfig.java
+++ CRUD_App1(jdbc template)/src/main/java/org/example/config/SpringConfig.java
@@ -1,5 +1,5 @@
 package org.example.config;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
