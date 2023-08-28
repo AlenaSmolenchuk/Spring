b60789b76c4b2d31ffd9aa7fa508b569cd603bf0
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-08-28 21:25:09 +0300

    Add files via upload

---
+++ Project1_Spring/src/main/sql/tables.sql
@@ -0,0 +1,13 @@
+create table person (
+                        id integer AUTO_INCREMENT PRIMARY KEY,
+                        full_name varchar(124) UNIQUE,
+                        birthday_year integer NOT NULL
+);
+
+create table book (
+                      id integer AUTO_INCREMENT PRIMARY KEY,
+                      person_id integer REFERENCES person(id) ON DELETE SET NULL,
+                      title varchar(100) NOT NULL ,
+                      author varchar(100) NOT NULL ,
+                      year integer NOT NULL
+);
