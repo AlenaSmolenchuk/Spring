4ca584e4c2b0d66b9c2616fcfb127b1d9db846bc
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-08-29 17:23:02 +0300

    Update tables.sql

--- Project1_Spring/src/main/sql/tables.sql
+++ Project1_Spring/src/main/sql/tables.sql
@@ -8,6 +8,6 @@ create table book (
                       id integer AUTO_INCREMENT PRIMARY KEY,
                       person_id integer REFERENCES person(id) ON DELETE SET NULL,
                       title varchar(100) NOT NULL,
-                      author varchar(100) NOT NULL ,
+                      author varchar(100) NOT NULL,
                       year integer NOT NULL
 );
