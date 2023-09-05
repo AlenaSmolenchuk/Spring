b29a4e27e043fd4564b57dbfa1b561e0fbd61de9
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-09-05 16:33:01 +0300

    Update BookDAO.java

--- Project1_Spring/src/main/java/org/example/dao/BookDAO.java
+++ Project1_Spring/src/main/java/org/example/dao/BookDAO.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @Component
 public class BookDAO {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate; 
 
     @Autowired
     public BookDAO(JdbcTemplate jdbcTemplate) {
