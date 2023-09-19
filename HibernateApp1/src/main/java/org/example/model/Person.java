5692efaad61c00108a818c9e193ed877a76a8905
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-09-19 11:31:37 +0300

    Update Person.java

--- HibernateApp1/src/main/java/org/example/model/Person.java
+++ HibernateApp1/src/main/java/org/example/model/Person.java
@@ -1,9 +1,6 @@
 package org.example.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "Person")
@@ -11,6 +8,7 @@ public class Person {
 
     @Id
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "name")
@@ -21,8 +19,7 @@ public class Person {
 
     public Person() {}
 
-    public Person(int id, String name, int age) {
-        this.id = id;
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
@@ -49,5 +46,10 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + ": " + this.name + ", " + this.age;
     }
 }
