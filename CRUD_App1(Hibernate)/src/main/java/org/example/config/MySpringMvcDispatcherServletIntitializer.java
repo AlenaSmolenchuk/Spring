945934157d707d77131c8059e19bad4bb5c88947
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-10-18 18:30:31 +0300

    Update MySpringMvcDispatcherServletIntitializer.java

--- CRUD_App1(Hibernate)/src/main/java/org/example/config/MySpringMvcDispatcherServletIntitializer.java
+++ CRUD_App1(Hibernate)/src/main/java/org/example/config/MySpringMvcDispatcherServletIntitializer.java
@@ -6,7 +6,7 @@ import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class MySpringMvcDispatcherServletIntitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-    
+     
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return null;
